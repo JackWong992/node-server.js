2d41680357ee013fa20d27a235491c7fcbc617aa
JackWong992 <jackwong992@163.com>
2018-07-07 08:37:31 +0800

    update server.js

---
+++ server.js
@@ -0,0 +1,58 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('得到 HTTP 路径\n' + path)
+  if(path=='/' ){
+     response.write('***访问的是正确的***\n')
+     response.end()
+   }else if(path == '/index.html' ){
+     response.setHeader('Content-Type', 'text/html; charset=utf-8')
+     response.write('<!DOCTYPE>\n'+
+	'<html>'+
+	'<head><link rel="stylesheet" href="/style.css" ></head>'+
+	'<body><h1>hello Node.JS</h1><script src="/main.js"></script> </body>'+
+	'</html>')
+     response.end()
+    }else if(path == '/style.css'){
+	response.setHeader('Content-Type','text/css;charset=utf-8')
+  	response.write('body{background: #ddd; } h1{color: red;}' )
+	response.end()
+  }else if(path == '/main.js'){
+	response.setHeader('Content-Type','text/javascript; charset=utf-8')
+ 	response.write('alert("***哈哈哈,已经连接上了main.js***")')
+	response.end()
+ }	 
+   else{
+     response.statusCode = 404
+     response.write('***出错了,访问的不是根地址***\n')
+     response.end()
+ }
+	
+
+
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
